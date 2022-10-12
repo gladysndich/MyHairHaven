96ef31189ec1dbd4f4084a4c5b7ed5a64982b3d3
Peter Kimani <peterjajakim@gmail.com>
2022-10-12 16:57:14 +0300

    CREATED HAIRSTYLE COMPONENTS

---
+++ frontend/hair-front/src/components/HairstyleContainer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Hairstyle from "./Hairstyle";
+
+
+function HairstylesContainer() {
+  const [hairstyles, setHairstyleList] = useState([]);
+    useEffect(() => {
+    fetch('http://localhost:3000/hairstyles')
+    .then(res => res.json())
+    .then(hairstylesData => {
+      setHairstyleList(hairstylesData)
+      });
+  }, []);
+
+  const hairstylesCard = hairstyles.map( (hairstyle)=> (<Hairstyle key={hairstyle.id} pImage={hairstyle.image}  pName={hairstyle.name}  pPrice={hairstyle.price} pDuration={hairstyle.duration}/>))
+
+  
+  return (
+    <div className="container-fluid">
+     {hairstylesCard}
+    </div>
+  );
+}
+
+export default HairstylesContainer;
+  
