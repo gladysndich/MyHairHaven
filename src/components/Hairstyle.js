388f8251f6495dfbfe55fdece12ffbd02fb0f99a
gladysndich <itsgladysbrian@gmail.com>
2022-10-13 23:33:45 +0300

    Hairstyle cards styled

--- src/components/Hairstyle.js
+++ src/components/Hairstyle.js
@@ -4,24 +4,56 @@ import Card from 'react-bootstrap/Card';
 
 
 function Hairstyle({ pTitle, pImage, pPrice, pDuration}) {
+
+  var stylingCard = {
+    "box-shadow": "0 4px 8px 0 rgba(0,0,0,0.2)",
+  transition: "0.3s",
+  padding: "2px 16px",
+  width: "300px",
+  "font-family": "Itim",
+  "background-color": "#000",
+  margin: "1rem",
+  display: "inline-block",
+  "vertical-align": "middle"
+}
+
+var bookHairstyleButton = {
+  border: "none",
+outline: "0",
+display: "inline-block",
+padding: "8px",
+"color": "#000",
+"background-color": "#FFFFFF",
+"text-align": "center",
+cursor: "pointer",
+width: "100%",
+"font-size": "18px",
+"font-family": "Itim"
+}
+
+var stylingFont1 = {
+  "font-family": "Itim",
+  "font-size": "30px",
+  color: "#FFFFFF"
+}
+
+var stylingFont = {
+  "font-family" : "Itim",
+  color: "#FFFFFF"
+}
+
     return (
-        <div class= "scard" >
-        <div class="col">
-          <div class="col-sm-10">
-            <div class="card" style={{ width: '28rem' }}>
-              <div class="card-body">
-                <h5 class="card-name">TITLE: {pTitle}</h5>
-                <Card.Img variant="top" src= {pImage} style={{height: 200 + 'px', width:400 + 'px'}}/>
-                <h5 class="card-city">PRICE: {pPrice}</h5>
-                <h5 class="card-genres">DURATION(IN HOURS): {pDuration}</h5>
-                <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-               </div>
-              </div>
-            </div>
-          </div> 
-          </div>
-          </div>
+      <div style={stylingCard}>
+            <h5 style={stylingFont1}>TITLE: {pTitle}</h5>
+            <Card.Img variant="top" src= {pImage} style={{height: 200 + 'px', width:200 + 'px'}}/>
+            <h5 style={stylingFont}>PRICE: {pPrice}</h5>
+            <h5 style={stylingFont}>DURATION(IN HOURS): {pDuration}</h5>
+            <p><button style={bookHairstyleButton}>Book Hairstyle</button></p>
+        </div>
+
     );
+
+    
 }
 
 export default Hairstyle;
